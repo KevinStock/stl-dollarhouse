057006cc4949673a1e54a34baa3f4eb46a56d12b
kevin <kevin@kevinstock.net>
2019-03-31 09:19:46 -0400

    changed handling of property list to allow additional information to be displayed

--- app.py
+++ app.py
@@ -6,16 +6,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-  return render_template('index.html', gkey=gkey, coordinates=coordinates)
+  return render_template('index.html', gkey=gkey, properties=properties)
 
-def getCoordinates():
-  properties = pd.read_csv("data/properties.csv")
-  coordinates = []
-  for index, row in properties.iterrows():
-      coordinates.append(tuple((row["Latitude"], row["Longitude"])))
-  return coordinates
+def getProperties():
+  data = pd.read_csv("data/properties.csv")
+  properties = data.to_dict('records')
+  return properties
 
-coordinates = getCoordinates()
+properties = getProperties()
 
 if __name__ == '__name__':
   app.run(debug=True, port=3000)
